5749ef9c6b05bb2020e0e2cb84cfd8817884b300
Julien C <julien.castelain@liferay.com>
2017-03-09 14:26:42 +0100

    Add fake dom elements

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 const express = require('express');
 const app = express();
-const jsdom = require('jsdom');
 
-// document needs to be global because metal expects that
-// i.e.
-// When "requiring" metal-component, this happens:
-//
-// ./node_modules/metal-dom/lib/features.js:85
-// features.animationElement_ = document.createElement('div');
-//                              ^
-// ReferenceError: document is not defined
-global.document = jsdom.jsdom();
-global.window = document.defaultView;
+global.document = require('./browser').document;
+global.document.body = document.createElement();
+global.window = {};
 
-// not really sure how to use metal from nodejs
 const metal = {
   core: require('metal').core,
   component: require('metal-component'),
@@ -28,7 +19,6 @@ const elementVoid = incrementalDomString.elementVoid;
 const text = incrementalDomString.text;
 const patch = incrementalDomString.patch;
 const getOutput = incrementalDomString.getOutput;
-
 
 class DemoComponent extends metal.component.Component {
   render() {
@@ -53,13 +43,19 @@ class DemoComponent extends metal.component.Component {
 }
 DemoComponent.RENDERER = metal.incrementalDom.IncrementalDomRenderer;
 
-const container = document.createElement('div');
-container.setAttribute('id', 'main-container');
-document.body.appendChild(container);
 
 app.get('/', (req, res) => {
-  new DemoComponent({element: container}).render();
-  const html = document.documentElement.outerHTML;
+  new DemoComponent({}).render();
+  const html =
+`
+<!doctype html>
+<html>
+  <head></head>
+  <body>
+    ${getOutput()}
+  </body>
+</html>
+`;
   res.send(html);
 });
 
