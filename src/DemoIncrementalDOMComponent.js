06e498c4d915444537b300dc199861d17b82bf3d
Julien C <julien.castelain@liferay.com>
2017-03-28 21:06:41 +0200

    Move IncrementalDOM component to src dir

--- src/DemoIncrementalDOMComponent.js
+++ src/DemoIncrementalDOMComponent.js
@@ -1,20 +1,19 @@
 const metal = {
-  core: require('metal').core,
   component: require('metal-component'),
   incrementalDom: require('metal-incremental-dom')
 };
 
-const incrementalDomString = require('./vendor/virtual_elements');
+const incrementalDomString = require('../vendor/virtual_elements');
 const elementOpen = incrementalDomString.elementOpen;
 const elementClose = incrementalDomString.elementClose;
 const text = incrementalDomString.text;
 const patch = incrementalDomString.patch;
+
 global.IncrementalDOM = incrementalDomString;
 
-// Demo IncrementalDOM component
 class DemoIncrementalDOMComponent extends metal.component.Component {
   render() {
-    return patch(this.element, function () {
+    return patch({}, function () {
       elementOpen('div');
         elementOpen('h3');
           text('Metal.js Demos');
@@ -50,13 +49,5 @@ class DemoIncrementalDOMComponent extends metal.component.Component {
   }
 }
 DemoIncrementalDOMComponent.RENDERER = metal.incrementalDom.default;
-
-// This will break since window.customElements is not
-// defined on the "server" (even if jsdom is used)
-// metal.core.defineWebComponent('metal-demo-incdom', DemoIncrementalDOMComponent);
-
-// We could "shim" the CustomElements API:
-// https://html.spec.whatwg.org/multipage/scripting.html#custom-elements-api
-
 module.exports = DemoIncrementalDOMComponent;
 
