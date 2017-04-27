7a5a899250bdbd1a8aa305223666162a3105c1c1
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2017-04-27 15:04:51 -0700

    Removes code that is on metal now

--- index.js
+++ index.js
@@ -6,23 +6,9 @@ global.document = jsdom.jsdom();
 global.window = global.document.defaultView;
 
 const Component = require('metal-component').default;
-const IncrementalDomRenderer = require('metal-incremental-dom').default;
-
 const AJSX = require('./dist/src/jsx/A').default;
 const ASoy = require('./dist/src/soy/A').default;
 const AIDom = require('./dist/src/idom/A').default;
-
-Component.renderToString = function(ctor, data) {
-  let stack = [];
-  let patch = IncrementalDOM.patch;
-  IncrementalDOM.patch = function() {
-    let currentElement = patch.apply(null, arguments);
-    stack.push(currentElement.innerHTML);
-    IncrementalDOM.patch = patch;
-  };
-  Component.render(ctor, data);
-  return stack[0];
-};
 
 app.get('/', (req, res) => {
   res.send(Component.renderToString(AIDom));
